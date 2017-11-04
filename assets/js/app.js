1ed6c5b36e7f1bd8d2a5beed4c50afbde9890b9d
pr0tools <33316308+pr0tools@users.noreply.github.com>
2017-11-04 16:28:04 +0100

    Init 1.0

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+
+var request = new XMLHttpRequest();
+request.open('GET', 'links.json', true);
+
+request.onerror = function() {
+    alert('Ups! Da is wohl was kaputt. Versuche es später erneut!');
+};
+request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+        var data = JSON.parse(this.response);
+        data.forEach(function(item) {
+            $('.list').append('<div class="item">\
+                <a href="'+item.link+'"><div class="title">'+item.title+'</div></a>\
+                <div class="description">'+item.description+'</div>\
+            </div>');
+        });
+        var jets = new Jets({
+          searchTag: '#jetsSearch',
+          contentTag: '.list'
+      });
+    } else {
+        request.onerror();
+    }
+};
+request.send();
